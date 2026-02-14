5f199a8bad663bc64c3e538439b6e5207f5907d3
Tejumoluwa Kolawole <tejumoluwa.kolawole@gmail.com>
2026-02-14 12:08:11 -0600

    html and css

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function rollDice() {
-    var rollValue = Math.floor(Math.random() * 6) +1;
-
-    var diceMat = document.getElementById("diceResult");
+    /* Stores random number from 1 through 6*/
+    let rollValue = Math.floor(Math.random() * 6) +1;
+    /*Returns diceResult */
+    let diceMat = document.getElementById("diceResult");
 
     diceMat.innerHTML = rollValue;
 }
 
+/* Automatically rolls dice as the website loads */
 window.onload = rollDice();
